18ee85c401d94b2a1144325101d7469895c321e7
Josh Kingsley <josh@joshkingsley.me>
2022-10-21 17:02:12 +0300

    Add git repo

--- modules/nixos/boxvm/configuration.nix
+++ modules/nixos/boxvm/configuration.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ pkgs, config, inputs, ... }:
 
 {
   imports = [ "${inputs.nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix" ];
@@ -30,8 +30,34 @@
     }
   ];
 
-  services.nginx = {
+  services.openssh.extraConfig = ''
+    PrintLastLog no
+  '';
+
+  programs.git = {
     enable = true;
-    virtualHosts."localhost" = { root = "${inputs.self}/site"; };
+    config = { init.defaultBranch = "main"; };
+  };
+
+  users.users.git = {
+    uid = config.ids.uids.git;
+    group = "git";
+    shell = "${pkgs.git}/bin/git-shell";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFfcOdH0DX1wM+1UvZ3nBeKuGLyXv+TcHxFyONUaxhhb josh@sparrowhawk"
+    ];
+  };
+
+  users.groups.git.gid = config.ids.gids.git;
+
+  system.activationScripts = {
+    boxInitialize.text = ''
+      if [[ ! -d /box.git ]]; then
+        mkdir -p /box.git
+        cd /box.git
+        ${pkgs.git}/bin/git init --bare
+        chown --recursive git:git /box.git
+      fi
+    '';
   };
 }
